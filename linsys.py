8751a791b224d6634c307322091bcaad441ed581
Karthik Rao <karthikrao@Karthiks-Air.Home>
2017-06-26 16:43:03 +0530

    Initial commit

---
+++ linsys.py
@@ -0,0 +1,261 @@
+from decimal import Decimal, getcontext
+from copy import deepcopy
+
+from vector import Vector
+from hyperplane import Hyperplane
+
+getcontext().prec = 30
+
+
+class LinearSystem(object):
+
+    ALL_PLANES_MUST_BE_IN_SAME_DIM_MSG = 'All planes in the system should live in the same dimension'
+    NO_SOLUTIONS_MSG = 'No solutions'
+    INF_SOLUTIONS_MSG = 'Infinitely many solutions'
+
+    def __init__(self, planes):
+        try:
+            d = planes[0].dimension
+            for p in planes:
+                assert p.dimension == d
+
+            self.planes = planes
+            self.dimension = d
+
+        except AssertionError:
+            raise Exception(self.ALL_PLANES_MUST_BE_IN_SAME_DIM_MSG)
+
+    def swap_rows(self, row1, row2):
+        self[row1],  self[row2] = self[row2], self[row1]
+
+    def multiply_coefficient_and_row(self, coefficient, row):
+        n = self[row].normal_vector
+        k = self[row].constant_term
+
+        new_normal_vector = n.times_scalar(coefficient)
+        new_constant_term = k * coefficient
+
+        # Create new plane to avoid manually setting basepoints again after multiplication
+        self[row] = Hyperplane(normal_vector=new_normal_vector, constant_term=new_constant_term)
+
+    def add_multiple_times_row_to_row(self, coefficient, row_to_add, row_to_be_added_to):
+        n1 = self[row_to_add].normal_vector
+        k1 = self[row_to_add].constant_term
+        n2 = self[row_to_be_added_to].normal_vector
+        k2 = self[row_to_be_added_to].constant_term
+
+        new_normal_vector = n1.times_scalar(coefficient).plus(n2)
+        new_constant_term = (k1 * coefficient) + k2
+
+        # Create new plane to avoid manually setting basepoints again after multiplication
+        self[row_to_be_added_to] = Hyperplane(normal_vector=new_normal_vector, constant_term=new_constant_term)
+
+    def indices_of_first_nonzero_terms_in_each_row(self):
+        num_equations = len(self)
+        num_variables = self.dimension
+
+        indices = [-1] * num_equations
+
+        for i,p in enumerate(self.planes):
+            try:
+                indices[i] = p.first_nonzero_index(p.normal_vector.coordinates)
+            except Exception as e:
+                if str(e) == Hyperplane.NO_NONZERO_ELTS_FOUND_MSG:
+                    continue
+                else:
+                    raise e
+
+        return indices
+
+    def __len__(self):
+        return len(self.planes)
+
+    def __getitem__(self, i):
+        return self.planes[i]
+
+    def __setitem__(self, i, x):
+        try:
+            assert x.dimension == self.dimension
+            self.planes[i] = x
+
+        except AssertionError:
+            raise Exception(self.ALL_PLANES_MUST_BE_IN_SAME_DIM_MSG)
+
+    def compute_triangular_form(self):
+        system = deepcopy(self)
+
+        num_equations = len(system)
+        num_variables = system.dimension
+        # Variable number in an equation starting at 0
+        j = 0
+
+        # Loop on one equation at a time
+        for i in range(num_equations):
+            while j < num_variables:
+                c = MyDecimal(system[i].normal_vector[j])
+                if c.is_near_zero():
+                    swap_succeded = system.swap_with_row_below_for_nonzero_coefficient_if_able(i, j)
+                    if not swap_succeded:
+                        j += 1
+                        continue
+
+                system.clear_coefficients_below(i, j)
+                j += 1
+                break
+
+        return system
+
+    def swap_with_row_below_for_nonzero_coefficient_if_able(self, row, col):
+        num_equations = len(self)
+
+        for k in range(row + 1, num_equations):
+            coefficient = MyDecimal(self[k].normal_vector[col])
+            if not coefficient.is_near_zero():
+                self.swap_rows(row, k)
+                return True
+
+        return False
+
+    def clear_coefficients_below(self, row, col):
+        num_equations = len(self)
+        beta = MyDecimal(self[row].normal_vector[col])
+
+        for k in range(row + 1, num_equations):
+            n = self[k].normal_vector
+            gamma = n[col]
+            alpha = - gamma/beta
+            self.add_multiple_times_row_to_row(alpha, row, k)
+
+    def compute_rref(self):
+        tf = self.compute_triangular_form()
+        num_equations = len(tf)
+        pivot_indices = tf.indices_of_first_nonzero_terms_in_each_row()
+
+        for i in range(num_equations - 1, -1, -1):
+            j = pivot_indices[i]
+            if j < 0:
+                continue
+            tf.scale_row_to_make_coefficient_equal_one(i, j)
+            tf.clear_coefficients_above(i ,j)
+
+        return tf
+
+    def scale_row_to_make_coefficient_equal_one(self, row, col):
+        # Multiply equation by leading variable's coefficient's reciprocal
+        n = self[row].normal_vector
+        beta = Decimal(1.0) / n[col]
+        self.multiply_coefficient_and_row(beta, row)
+
+    def clear_coefficients_above(self, row, col):
+        # Reverse loop the equations and clear coefficients from last variable to first
+        for k in range(row - 1, -1, -1):
+            n = self[k].normal_vector
+            alpha = -(n[col])
+            self.add_multiple_times_row_to_row(alpha, row, k)
+
+    def compute_solution(self):
+        try:
+            return self.do_gaussian_elimination_and_parameterize_solution()
+        except Exception as e:
+            if str(e) == self.NO_SOLUTIONS_MSG:
+                raise str(e)
+            else:
+                raise e
+
+    def do_gaussian_elimination_and_parameterize_solution(self):
+        rref = self.compute_rref()
+
+        rref.raise_exception_if_contradictory_equation()
+
+        direction_vectors = rref.extract_direction_vectors_for_parameterization()
+        basepoint = rref.extract_basepoint_for_parameterization()
+
+        return Parameterization(basepoint, direction_vectors)
+
+    def raise_exception_if_contradictory_equation(self):
+        # Raise exception for k = 0 equation
+        for p in self.planes:
+            try:
+                p.first_nonzero_index(p.normal_vector)
+            except Exception as e:
+                if str(e) == Hyperplane.NO_NONZERO_ELTS_FOUND_MSG:
+                    constant_term = MyDecimal(p.constant_term)
+                    if constant_term.is_near_zero():
+                        raise Exception(self.NO_SOLUTIONS_MSG)
+                else:
+                    raise e
+
+    def extract_direction_vectors_for_parameterization(self):
+        num_variables = self.dimension
+        pivot_indices = self.indices_of_first_nonzero_terms_in_each_row()
+        # Set of free vars to work on to get direction vectors
+        free_variables_indices = set(range(num_variables)) - set(pivot_indices)
+
+        direction_vectors = []
+
+        for free_var in free_variables_indices:
+            vector_coords = [0] * num_variables
+            # Vector coordinate for free var set as 1 as last equation
+            # will always be of form z = z for free var
+            vector_coords[free_var] = 1
+            for i, p in enumerate(self.planes):
+                pivot_var = pivot_indices[i]
+                if pivot_var < 0:
+                    break
+                # Negating the coefficient of free var as its sign changes
+                # on moving to RHS
+                vector_coords[pivot_var] = -p.normal_vector[free_var]
+            direction_vectors.append(Vector(vector_coords))
+
+        return direction_vectors
+
+    def extract_basepoint_for_parameterization(self):
+        num_variables = self.dimension
+        pivot_indices = self.indices_of_first_nonzero_terms_in_each_row()
+
+        basepoint_coords = [0] * num_variables
+        for i, p in enumerate(self.planes):
+            pivot_var = pivot_indices[i]
+            if pivot_var < 0:
+                break
+            basepoint_coords[pivot_var] = p.constant_term
+
+        return Vector(basepoint_coords)
+
+    def raise_exception_if_too_few_pivots(self):
+        pivot_indices = tf.indices_of_first_nonzero_terms_in_each_row()
+        num_pivots = sum([1 if index >= 0 else 0 for index in pivot_indices])
+
+        num_variables = self.dimension
+
+        if num_pivots < num_variables:
+            raise Exception(self.INF_SOLUTIONS_MSG)
+
+    def __str__(self):
+        ret = 'Linear System:\n'
+        temp = ['Equation {}: {}'.format(i+1,p) for i,p in enumerate(self.planes)]
+        ret += '\n'.join(temp)
+        return ret
+
+
+class MyDecimal(Decimal):
+    def is_near_zero(self, eps=1e-10):
+        return abs(self) < eps
+
+
+class Parameterization(object):
+    BASRPT_AND_DIR_VECTORS_MUST_BE_IN_SAME_DIM_MSG = (
+        'The basepoint and direction vectors should all live in the same dimension'
+    )
+
+    def __init__(self, basepoint, direction_vectors):
+        self.basepoint = basepoint
+        self.direction_vectors = direction_vectors
+        self.dimension = self.basepoint.dimension
+
+        try:
+            for v in direction_vectors:
+                assert v.dimension == self.dimension
+        except AssertionError:
+            raise Exception(self.BASRPT_AND_DIR_VECTORS_MUST_BE_IN_SAME_DIM_MSG)
+
